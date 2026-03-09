7dd1ee37523e7a871197f1171192241c37c4a3fd
suvraaj nandwani <167646900+SUVRAAJ@users.noreply.github.com>
2026-03-09 18:21:59 +0530

    a comment was added in

--- client/src/components/ProtectedRoutes.jsx
+++ client/src/components/ProtectedRoutes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Navigate} from 'react-router-dom'
-
+//we take an element and that can only be visied if jwt tpken is assigned to it
 const ProtectedRoutes = ({children}) => {
   const token= localStorage.getItem('token')
     //if not logged in then navigate back to lgin page
