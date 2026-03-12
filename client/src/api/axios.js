2498529bf0bdbb9bcbbbf85bd2f6aa3ab27422ca
suvraaj nandwani <167646900+SUVRAAJ@users.noreply.github.com>
2026-03-13 01:12:03 +0530

    updated base URL for deployment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API= axios.create({
-    baseURL:'http://localhost:3000/api'
+    baseURL:'https://dude-dues.onrender.com/api"'
 })
 
 API.interceptors.request.use( (config)=>{
