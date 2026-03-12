b584cc66ee15ba45a94a671c084fa35f6936f4d0
suvraaj nandwani <167646900+SUVRAAJ@users.noreply.github.com>
2026-03-13 01:38:00 +0530

    fixed error in axios

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API= axios.create({
-    baseURL:'https://dude-dues.onrender.com/api"'
+    baseURL:'https://dude-dues.onrender.com/api'
 })
 
 API.interceptors.request.use( (config)=>{
